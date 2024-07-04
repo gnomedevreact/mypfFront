a4083549df3c46c7f9be47ea366ea0f3b5997049
Мирон Бриньковский <gnomedevreact@gmail.com>
2024-07-04 15:47:25 +0200

    speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "sonner";
 import { Header } from "@/components/shared/Header/Header";
 import { SideBar } from "@/components/shared/SideBar/SideBar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         {children}
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
